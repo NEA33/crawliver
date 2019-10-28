2a885206e22f9006433b85991a475d1b402858ae
SingularityA <singularity.m101@gmail.com>
2019-10-28 16:08:28 +0300

    Updated database service interface

--- src/main/java/ru/spbu/crawliver/db/DatabaseService.java
+++ src/main/java/ru/spbu/crawliver/db/DatabaseService.java
@@ -4,7 +4,11 @@ import edu.uci.ics.crawler4j.crawler.Page;
 
 public interface DatabaseService {
 
-    void store(Page webPage);
+    void store(Page page, String domainRestriction);
+
+    boolean isNew(Page page);
+
+    boolean isNew(String url);
 
     void close();
 }
