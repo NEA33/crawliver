4698257ed975d246d59f8e7d82ff609659ec11b7
SingularityA <singularity.m101@gmail.com>
2019-10-28 16:14:21 +0300

    Added tasks for db storing tasks

--- src/test/java/ru/spbu/crawliver/dbstats/Tasks.java
+++ src/test/java/ru/spbu/crawliver/dbstats/Tasks.java
@@ -1,23 +1,51 @@
 package ru.spbu.crawliver.dbstats;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import ru.spbu.crawliver.controllers.CrawlerController;
+import ru.spbu.crawliver.helpers.CrawlerProperties;
+import ru.spbu.crawliver.helpers.DatabaseProperties;
+
+import java.io.IOException;
 
 public class Tasks {
 
+    private CrawlerController controller;
+    private CrawlerProperties spbuCrawlerProps;
+    private CrawlerProperties w3CrawlerProps;
+    private DatabaseProperties databaseProps;
+
+    @Before
+    public void init() throws IOException {
+        spbuCrawlerProps = new CrawlerProperties("spbu.crawler.properties");
+        w3CrawlerProps = new CrawlerProperties("w3school.crawler.properties");
+        databaseProps = new DatabaseProperties("database.properties");
+    }
+
     @Test
-    public void test() throws Exception {
-        CrawlerController controller = new PostgreSQLStoringCrawlerController(
-                System.getProperty("user.dir") + "/src/main/resources/temp",
-                "https://www.w3schools.com/sql/",
-                "www.w3schools.com/sql",
-                200,
-                1,
-                4,
-                "jdbc:postgresql://localhost/scraped",
-                "_USER_",
-                "_PASSWORD_"
+    public void testW3SchoolCrawler() throws Exception {
+        controller = new PostgreSQLStoringCrawlerController(
+                w3CrawlerProps,
+                databaseProps
         );
         controller.crawl();
+    }
+
+    @Ignore
+    @Test
+    public void testSpbuCrawler() throws Exception {
+        controller = new PostgreSQLStoringCrawlerController(
+                spbuCrawlerProps,
+                databaseProps
+        );
+        controller.crawl();
+    }
+
+    @Test
+    public void showProps() {
+        System.out.println(w3CrawlerProps);
+        System.out.println(spbuCrawlerProps);
+        System.out.println(databaseProps);
     }
 }
