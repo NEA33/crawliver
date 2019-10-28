d2d777e2be3b67b692d0330e391dd0450f3c63bd
SingularityA <singularity.m101@gmail.com>
2019-10-28 23:51:45 +0300

    Added sub domain field to migration

--- src/main/resources/db/migration/V01__initial_schema.sql
+++ src/main/resources/db/migration/V01__initial_schema.sql
@@ -1,17 +1,18 @@
 CREATE TABLE page(
   id BIGSERIAL,
-  url VARCHAR (4096) NOT NULL,
+  url VARCHAR (6144) NOT NULL,
   domain VARCHAR (128) NOT NULL,
+  sub_domain VARCHAR (128) NOT NULL,
   type VARCHAR (128),
   encoding VARCHAR (32),
-  language VARCHAR (32),
+  language VARCHAR (8),
   status_code INTEGER,
   bytes_length INTEGER,
   total_links INTEGER,
   external_links INTEGER,
   text_size INTEGER,
   html_size INTEGER,
-  title VARCHAR (128),
+  title VARCHAR (256),
   stamp TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
