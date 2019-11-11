21c0a560cf7f53e8407de6eb0b39296d60b183eb
SingularityA <singularity.m101@gmail.com>
2019-11-11 15:02:51 +0300

    Updated schema (added subdomain counter table)

--- src/main/resources/db/migration/V01__initial_schema.sql
+++ src/main/resources/db/migration/V01__initial_schema.sql
@@ -1,18 +1,27 @@
-CREATE TABLE page(
-  id BIGSERIAL,
-  url VARCHAR (6144) NOT NULL,
-  domain VARCHAR (128) NOT NULL,
-  sub_domain VARCHAR (128) NOT NULL,
-  type VARCHAR (128),
-  encoding VARCHAR (32),
-  language VARCHAR (8),
-  status_code INTEGER,
-  bytes_length INTEGER,
-  total_links INTEGER,
-  external_links INTEGER,
-  text_size INTEGER,
-  html_size INTEGER,
-  title VARCHAR (256),
-  stamp TIMESTAMP NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE page
+(
+    id               SERIAL,
+    domain           VARCHAR(256),
+    url              VARCHAR(5120) NOT NULL,
+    language         VARCHAR(4),
+    bytes_length     INTEGER,
+    total_links      INTEGER,
+    external_links   INTEGER,
+    sub_domain_links INTEGER,
+    text_size        INTEGER,
+    html_size        INTEGER,
+    title            VARCHAR(128),
+    stamp            TIMESTAMP     NOT NULL,
+    PRIMARY KEY (id)
 );
+
+CREATE TABLE domain
+(
+    id     SERIAL,
+    name   VARCHAR(128) NOT NULL,
+    number INTEGER,
+    PRIMARY KEY (id)
+);
+
+INSERT into domain(name, number)
+VALUES ('external', 0);
