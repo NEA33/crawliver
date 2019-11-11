76c68bc4115800b0d3e54ab3c5e0767eeb503761
SingularityA <singularity.m101@gmail.com>
2019-11-11 15:03:46 +0300

    Small fix (although may be other issues with this function)

--- src/main/java/ru/spbu/crawliver/helpers/UrlHelper.java
+++ src/main/java/ru/spbu/crawliver/helpers/UrlHelper.java
@@ -9,7 +9,7 @@ public class UrlHelper {
     public static String subDomain(String url, String domain) {
         final int nameBeginning = url.indexOf("//") + 2;
         final int nameEnd = url.indexOf(domain);
-        if (nameBeginning == nameEnd) return "";
+        if (nameBeginning >= nameEnd) return "";
         return url.substring(nameBeginning, nameEnd - 1);
     }
 
